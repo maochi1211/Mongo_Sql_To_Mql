bf6ac2bb887a436b3898373c98b7beb87960fd6d
Maochi <mcouch1211@gmail.com>
2023-11-20 13:24:53 +0800

    update Find_RealSevendays_Record

--- Find_RealSevendays_Record.js
+++ Find_RealSevendays_Record.js
@@ -1,13 +1,13 @@
+let collectionName = "cutDateTransformRecordNew";
+// let collectionName = "cutDateTransformRecordNew";
+
 // 函數：取得所有不重複的 driverId
 function getAllDriverIds() {
     // 使用 distinct 方法找出所有不重複的 driverId
-    return db.cutDateTransformRecordNew.distinct("driverId");
+    return db[collectionName].distinct("driverId");
 }
 
 var driverIds = getAllDriverIds();
-
-console.log('Driver IDs:', driverIds);
-console.log('===============');
 
 // 函數：取得某個 driverId 在某個日期之前的所有紀錄
 function getDriveTime(driverIds, date, pastDays) {
@@ -21,7 +21,7 @@ function getDriveTime(driverIds, date, pastDays) {
     console.log('startOfDay:', startOfDay);
     console.log('endOfDay:', endOfDay);
 
-    return db.cutDateTransformRecordNew.aggregate([
+    return db[collectionName].aggregate([
         {
             $match: {
                 driverId: { $in: driverIds }
@@ -50,15 +50,19 @@ function getDriveTime(driverIds, date, pastDays) {
 }
 
 var date = new Date('2023-10-07')
+var n = 0;
 cursor = getDriveTime(driverIds, date, 7)
 cursor.forEach(function(record) {
     var totalDriveTime = record.totalDriveTime ? record.totalDriveTime : 0;
     console.log('Driver ID:', record._id, 'Total drive time:', totalDriveTime);
+    n++;
 });
 
+console.log('Driver IDs:', driverIds);
+console.log('===============');
+console.log('Total number of driver:', n);
 console.log('===============');
 
-var date = new Date('2023-10-07')
 cursor2 = getDriveTime(['87'], date=date, pastDays=7)
 var totalDriveTime2 = cursor2.totalDriveTime ? cursor2.totalDriveTime : 0;
 console.log('Total drive time:', totalDriveTime2);
